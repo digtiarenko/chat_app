9b5af55264dd47c0e9d5b582009a297886a1e997
Valentyn Digtiarenko <baggins999@gmail.com>
2022-10-20 20:17:28 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ app.use('/api/message', messageRoutes);
 //Deployment================
 const basedir = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(basedir, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(basedir, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
